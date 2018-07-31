a80c0f5872a182860ba7e18172a1ab320780c832
raamshivajigoulikar <ramsgoulikar@gmail.com>
2018-07-31 19:40:42 +0530

    Update dateadd.py

--- dateadd.py
+++ dateadd.py
@@ -1,10 +1,12 @@
-start_date="2018-07-10"
-import datetime
-date_1=datetime.datetime.strptime(start_date,"%Y-%m-%d")
-print(date_1)
+import os
 import datetime
 from dateutil.relativedelta import relativedelta
+import math
 
+startdt=os.environ(['startdt'])
+enddt=os.environ(['enddt'])
+#startdt='2018-07-10'
+#enddt='2018-08-15'
 def dateadd(date, part ,value):
 
     if part=='year':
@@ -13,20 +15,22 @@ def dateadd(date, part ,value):
         result = date + (value * relativedelta(months = 1))
     elif part == 'day':
         result = date + (value * relativedelta(days = 1))
-    elif part == 'hour':
-        result = date + (value * relativedelta(hours = 1))
-    elif part == 'minute':
-        result = date + (value * relativedelta(minutes = 1))
-    elif part == 'second':
-        result = date + (value * relativedelta(seconds = 1))
     return result
-    
-    date_2=dateadd(date_1.date(),'month',-6)
-    
-    
-    dateadd(dateadd(date_4,'month',1),'day',-1)
-    
-    start_dt=dateadd(date_1,'month',-6)
-    start_dt=datetime.date(start_dt.year,start_dt.month,1)
-    end_dt=dateadd(dateadd(start_dt,'month',1),'day',-1)
-    start_dt=str(start_dt)
+#Creating Date Variables for Query
+startdt=datetime.datetime.strptime(startdt,"%Y-%m-%d")
+enddt=datetime.datetime.strptime(enddt,"%Y-%m-%d")
+start_dt=dateadd(startdt,'month',-6)
+end_dt=dateadd(dateadd(enddt,'month',-6),'month',1)
+end_dt=str(dateadd(datetime.date(end_dt.year,end_dt.month,1),'day',-1))
+start_dt=str(datetime.date(start_dt.year,start_dt.month,1))
+#Creating Quarter Variables
+start_qrt=dateadd(dateadd(startdt,'month',1),'month',-6)
+start_qrt=str(start_qrt.year) + "Q" + str(math.ceil((start_qrt.month/3)))
+pull_qrt=dateadd(dateadd(startdt,'month',1),'month',0)
+pull_qrt=str(pull_qrt.year) + "Q" + str(math.ceil((pull_qrt.month/3)))
+
+
+print(start_dt)
+print(end_dt)
+print(start_qrt)
+print(pull_qrt)
