281e9bbefb9a08ebbb4150b6eb93b8d6b38e5c8c
raamshivajigoulikar <ramsgoulikar@gmail.com>
2018-08-17 10:41:01 +0530

    Update Pandas_Transformations.py

--- Pandas_Transformations.py
+++ Pandas_Transformations.py
@@ -33,3 +33,73 @@ def times100(x):
     else:
         return
 df.applymap(times100)
+
+#If Else if and multiple column conditions.
+import pandas as pd
+import numpy as np
+raw_data = {'student':['A','B','C','D','E'],
+        'score': [100, 96, 80, 105,156], 
+    'height': [7, 4,9,5,3],
+    'trigger1' : [84,95,15,78,16],
+    'trigger2' : [99,110,30,93,31],
+    'trigger3' : [114,125,45,108,46]}
+
+df2 = pd.DataFrame(raw_data, columns = ['student','score', 'height','trigger1','trigger2','trigger3'])
+
+print(df2)
+
+
+I have a dataframe like below.
+
+import pandas as pd
+import numpy as np
+raw_data = {'student':['A','B','C','D','E'],
+        'score': [100, 96, 80, 105,156], 
+    'height': [7, 4,9,5,3],
+    'trigger1' : [84,95,15,78,16],
+    'trigger2' : [99,110,30,93,31],
+    'trigger3' : [114,125,45,108,46]}
+
+df2 = pd.DataFrame(raw_data, columns = ['student','score', 'height','trigger1','trigger2','trigger3'])
+
+print(df2)
+I need to derive Flag column based on multiple conditions.
+
+i need to compare score and height columns with trigger 1 -3 columns.
+
+Flag Column:
+
+if Score greater than equal trigger 1 and height less than 8 then Red --
+
+if Score greater than equal trigger 2 and height less than 8 then Yellow --
+
+if Score greater than equal trigger 3 and height less than 8 then Orange --
+
+if height greater than 8 then leave it as blank
+
+How to write if else conditions in pandas dataframe and derive columns?
+
+Expected Output
+
+  student  score  height  trigger1  trigger2  trigger3    Flag
+0       A    100       7        84        99       114  Yellow
+1       B     96       4        95       110       125     Red
+2       C     80       9        15        30        45     NaN
+3       D    105       5        78        93       108  Yellow
+4       E    156       3        16        31        46  Orange
+
+
+
+
+def flag_df(df):
+
+    if (df['trigger1'] <= df['score'] < df['trigger2']) and (df['height'] < 8):
+        return 'Red'
+    elif (df['trigger2'] <= df['score'] < df['trigger3']) and (df['height'] < 8):
+        return 'Yellow'
+    elif (df['trigger3'] <= df['score']) and (df['height'] < 8):
+        return 'Orange'
+    elif (df['height'] > 8):
+        return np.nan
+
+df2['Flag'] = df2.apply(flag_df, axis = 1)
